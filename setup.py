eff60e8552efa03465b86acb49793579440cbf96
majkelx <mkalusz@camk.edu.pl>
2020-06-04 17:24:34 +0200

    project structore 2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='teda_fits',
+    version='0.9',
+    packages=['teda', 'teda.views', 'teda.models', 'teda.widgets', 'teda.painterShapes'],
+    url='https://github.com/majkelx/teda',
+    license='MIT',
+    author='Akond Lab',
+    author_email='',
+    description='TeDa FITS Viewer'
+)
